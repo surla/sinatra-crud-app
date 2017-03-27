32c3f87757ab7cd26924416901626518271a9388
Joseph Surla <josephsurla@gmail.com>
2017-03-26 19:17:46 -0700

    Add route and view for login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   get '/users/home' do
-    @user = User.all
+    @user = User.find(email: params[:email])
     erb :'/users/home'
   end
 
@@ -9,4 +9,21 @@ class UsersController < ApplicationController
   @user = User.find(params[:id])
   erb :'/users/show'
   end
+
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  post '/login' do
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect :'/users/home'
+    else
+      redirect :'/signup'
+    end
+  end
+
+
 end
